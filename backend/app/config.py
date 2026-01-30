72926efa2f8937a3069c1f855e255741f27944ac
dayan iskhakov <dayan.iskhakov@gmail.com>
2026-01-31 01:42:06 +0500

    init db with run in dockerfile

--- backend/app/config.py
+++ backend/app/config.py
@@ -3,4 +3,6 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DATABASE_URL = 'sqlite+aiosqlite:///' + BASE_DIR / os.getenv("DATABASE_URL")
+db_name = os.getenv("DATABASE_URL", "db.sqlite3")
+
+DATABASE_URL = f'sqlite+aiosqlite:///{BASE_DIR / db_name}'
